bf822fd7298471664a3ea41cbbcb684324ef28c4
Vincent Jaquet <vincent.jaquet@edu.hefr.ch>
2022-05-20 22:45:36 +0200

    Check Raycast on a specific layer

--- Assets/Scripts/ARCursorPlace.cs
+++ Assets/Scripts/ARCursorPlace.cs
@@ -8,40 +8,42 @@ public class ARCursorPlace : MonoBehaviour
 {
     private readonly float SnapToValue = 0.1f;
     private readonly string SnapToTag = "__Block__";
-
+    private readonly string SnapToLayer = "Block";
     private readonly Color ColorSelected = Color.green;
     private readonly Color ColorNotSelected = Color.blue;
 
     public ARCursor cursor;
     public GameObject objectToPlace;
-    public ARRaycastManager raycastManager;
 
     private GameObject _next;
+    private int _snapToLayer;
 
     void Start()
     {
+        _snapToLayer = LayerMask.NameToLayer(SnapToLayer);
+
         _next = Instantiate(objectToPlace, Vector3.zero, Quaternion.identity);
+        _next.transform.SetLayerRecursively(_snapToLayer);
     }
 
     void Update()
     {
         // TODO: Handle duplicate on same position (Collider + Distance ?)
         // TODO: Handle Raycast on specific face
-        // TODO: Bypass Planes in Raycast (Using Mask)
 
         Vector3 nextPosition;
         Quaternion nextRotation = Quaternion.identity;
 
         foreach (var obj in GameObject.FindGameObjectsWithTag(SnapToTag))
         {
-            SetColor(obj.transform, ColorNotSelected);
+            obj.transform.SetColor(ColorNotSelected);
         }
 
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(Vector2.one / 2);
         Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity) && hit.transform.tag == SnapToTag)
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, 1 << _snapToLayer) && hit.transform.CompareTag(SnapToTag))
         {
-            SetColor(hit.transform, ColorSelected);
+            hit.transform.SetColor(ColorSelected);
 
             nextPosition = hit.transform.position + Vector3.up * SnapToValue / 2;
         }
@@ -58,17 +60,9 @@ public class ARCursorPlace : MonoBehaviour
         if (!TryGetTouchPosition(out _))
             return;
 
-        _next.GetComponentInChildren<Collider>().transform.tag = SnapToTag;
+        _next.transform.SetTag(SnapToTag);
         _next = Instantiate(objectToPlace, nextPosition, nextRotation);
-    }
-
-    void SetColor(Component component, Color color)
-    {
-        var renderer = component.GetComponentInChildren<Renderer>();
-        if (renderer && renderer.material)
-        {
-            renderer.material.color = ColorSelected;
-        }
+        _next.transform.SetLayerRecursively(_snapToLayer);
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions
+{
+    public static void SetColor(this Transform transform, Color color)
+    {
+        var renderer = transform.GetComponentInChildren<Renderer>();
+        if (renderer && renderer.material)
+        {
+            renderer.material.color = color;
+        }
+    }
+
+    public static void SetTag(this Transform transform, string tag)
+    {
+        transform.gameObject.tag = tag;
+        foreach (Transform child in transform)
+        {
+            SetTag(child, tag);
+        }
+    }
+}
